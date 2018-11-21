a938d3f5ba04bd0acc310acf5e4c6432d0d54d67
yzb@example.cn <yzb@example.cn>
2018-11-21 10:13:35 +0800

    update: vendor

--- project1/app.go
+++ project1/app.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+    "github.com/gorhill/cronexpr"
+    "github.com/bytom/protocol/bc"
     "fmt"
     "net/http"
 	"errors"
+    "time"
 )
 
 func Division(a, b float64) (float64, error) {
@@ -15,6 +18,38 @@ func Division(a, b float64) (float64, error) {
 }
 
 func main() {
+    var (
+        expr *cronexpr.Expression
+        err error
+        now time.Time
+        nextTime time.Time
+    )
+
+    // linux crontab
+    // 秒粒度, 年配置(2018-2099)
+    // 哪一分钟（0-59），哪小时（0-23），哪天（1-31），哪月（1-12），星期几（0-6）
+
+    // 每隔5分钟执行1次
+    if expr, err = cronexpr.Parse("*/5 * * * * * *"); err != nil {
+        fmt.Println(err)
+        return
+    }
+    
+    // 当前时间
+    now = time.Now()
+    // 下次调度时间
+    nextTime = expr.Next(now)
+
+    // 等待这个定时器超时
+    time.AfterFunc(nextTime.Sub(now), func() {
+        fmt.Println("被调度了:", nextTime)
+    })
+
+    time.Sleep(5 * time.Second)
+
+    var compareHash *bc.Hash
+    compareHash = compareHash
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "project1")
     })
